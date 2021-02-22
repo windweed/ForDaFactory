38544feb8f136b55d585a09f7aedcf014f1e4f93
czy_personalfedora33 <ziijchen@163.com>
2021-02-22 22:02:12 +0800

    update list: mergeTwoLists

--- src/list.cc
+++ src/list.cc
@@ -1,3 +1,8 @@
+/**
+ * @file 链表
+ * 21合并两个有序链表
+*/
+#include "listnode.h"
 #include <list>
 #include <forward_list>
 
@@ -5,6 +10,28 @@ using namespace std;
 
 class ListSolution {
 public:
+    /**
+     * @brief leetcode 12 merge-two-sorted-lists 合并有序链表 easy
+    */
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode prev_head = ListNode(-1);
+        ListNode* prev = &prev_head;
+
+        while (l1 && l2) {
+            if (l1->val <= l2->val) {
+                prev->next = l1;
+                l1 = l1->next;
+            } else {
+                prev->next = l2;
+                l2 = l2->next;
+            }
+            prev = prev->next;
+        }
+        prev->next = l1 ? l1 : l2;
+        return prev_head.next;
+    }
+
+
     /**
      * @brief 判断链表是否有环
     */
