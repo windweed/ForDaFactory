481ddfec0fab5a6bf01be5ff04f40cdb99cc7346
czy_personalfedora33 <ziijchen@163.com>
2021-02-24 13:07:39 +0800

    update other 26,27

--- src/others.cc
+++ src/others.cc
@@ -1,5 +1,5 @@
 /**
- * 1两数之和
+ * 1两数之和，26删除排序数组中的重复项，27移除元素
 */
 #include <vector>
 #include <unordered_map>
@@ -39,9 +39,47 @@ public:
         return {};
     }
 
+    /**
+     * @brief leetcode 26 remove-duplicates-from-sorted-array 删除排序数组中的重复项
+     *      要求原地删除，不能使用额外数组空间，返回移除后数组的新长度。要求O(1)额外空间。
+     *      easy
+     * @note 快慢指针
+    */
+    int removeDuplicates(vector<int>& nums) {
+        int len = nums.size();
+        if (len == 0) {
+            return 0;
+        }
 
+        int slow = 0, fast = 1;
+        for (; fast < len; fast++) {
+            if (nums[slow] != nums[fast]) {
+                slow++;
+                nums[slow] = nums[fast];
+            }
+        }
+        return slow + 1;
+    }
 
+    /**
+     * @brief leetcode 27 remove-element 移除元素 easy
+     *      给定一个数组和一个值，原地移除所有数值等于val的元素，并返回移除后数组的新长度
+    */
+    int removeElement(vector<int>& nums, int val) {
+        int len = nums.size();
+        if (len == 0) {
+            return 0;
+        }
 
+        int slow = 0, fast = 0;
+        for (; fast < len; fast++) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+        }
+        return slow;
+    }
 };
 
 
