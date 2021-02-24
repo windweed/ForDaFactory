95a7923b0f64f52557e0285facf2d14168043973
czy_personalfedora33 <ziijchen@163.com>
2021-02-24 13:07:05 +0800

    update string 28,35

--- src/strings.cc
+++ src/strings.cc
@@ -1,6 +1,6 @@
 /**
  * 字符串相关
- * 14最长公共前缀
+ * 14最长公共前缀，28实现strstr()，35搜索插入位置
 */
 #include <vector>
 #include <string>
@@ -38,5 +38,36 @@ public:
             idx++;
         }
         return str1.substr(0, idx);
+    }
+
+    /**
+     * @brief leetcode 28 implement-strstr 实现strstr easy
+     * @return 如果不存在，返回-1。
+    */
+    int myStrStr(string haystack, string needle) {
+        int hay_len = haystack.length();
+        int n_len = needle.length();
+
+        for (int i = 0; i < hay_len - n_len + 1; i++) {
+            if (haystack.substr(i, n_len) == needle) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * @brief leetcode 35 search-insert-position 搜索插入位置 easy
+     *      给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果不存在则
+     *      返回将会被插入的位置。
+     * @note 无重复元素。
+    */
+    int searchInsert(vector<int>& nums, int target) {
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.size();
     }
 };
