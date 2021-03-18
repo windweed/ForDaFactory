1175dc754267ffeacebc57767ad0db0c83c37add
czy_rhel7 <chenzhiyi@bjydzy.com>
2021-03-18 13:23:42 +0800

    BST comment

--- src/BST.cc
+++ src/BST.cc
@@ -11,8 +11,8 @@ class BSTSolution {
 public:
     /**
      * @brief leetcode 108 convert-sorted-array-to-binary-search-tree
-     *      将有序数组转换为二叉搜索树
-     * @note 将一个按照升序排列的有序数组，转换为一颗高度平衡的二叉搜索树
+     *        将有序数组转换为二叉搜索树 easy
+     *        将一个按照升序排列的有序数组，转换为一颗高度平衡的二叉搜索树
     */
     TreeNode* sortedArrayToBST(vector<int>& nums) {
         return createBST(nums, 0, nums.size() - 1);
@@ -23,7 +23,7 @@ public:
             return nullptr;
         }
 
-        int idx = (left + right) / 2; // 中间偏左
+        int idx = (left + right) / 2; // 总是选择中间偏左
         TreeNode* root = new TreeNode(nums[idx]);
         root->left = createBST(nums, left, idx - 1);
         root->right = createBST(nums, idx + 1, right);
@@ -35,7 +35,7 @@ public:
      *            BST的最近公共祖先 easy
      * @note 一个节点也可以是自己的祖先
      * @note 由于BST的特殊性，两个节点的最近公共祖先就是从根开始第一个满足 p < root < q
-     *       的节点。准确点说，是第一个不满足()
+     *       的节点。准确点说，是第一个不满足(p,q 均小于root || p,q均大于root)的节点。 
     */
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         TreeNode *node = root;
